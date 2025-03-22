930bfdd53e523fcb0aefe6caec21ab73c557631f
IannMarco13 <155080016+IannMarco13@users.noreply.github.com>
2025-03-22 15:13:58 -0400

    Update anim.js

--- anim.js
+++ anim.js
@@ -2,74 +2,39 @@
 var audio = document.querySelector("audio");
 var lyrics = document.querySelector("#lyrics");
 
-// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
-//var lyricsData = [
-//  { text: "At the time", time: 15 },
-//  { text: "The whisper of birds", time: 18 },
-//  { text: "Lonely before the sun cried", time: 27 },
-//  { text: "Fell from the sky", time: 32 },
-//  { text: "Like water drops", time: 33 },
-//  { text: "Where I'm now? I don't know why", time: 41 },
-//  { text: "Nice butterflies in my hands", time: 47 },
-//  { text: "Too much light for twilight", time: 54 },
-//  { text: "In the mood for the flowers love", time: 59 },
-//  { text: "That vision", time: 67 },
-//  { text: "Really strong, blew my mind", time: 72 },
-//  { text: "Silence Let me see what it was", time: 78 },
-//  { text: "I only want to live in clouds", time: 83 },
-//  { text: "Where I'm now? I don't know why", time: 91 },
-//  { text: "Nice butterflies in my hands", time: 97 },
-//  { text: "Too much light for twilight", time: 104 },
-//  { text: "In the mood for the flowers love", time: 108 },
-//  { text: "At the time", time: 144 },
-//  { text: "The whisper of birds", time: 148 },
-//  { text: "Lonely before the sun cried", time: 153 },
-//  { text: "Fell from the sky", time: 158 },
-//  { text: "Like water drops", time: 164 },
-//  { text: "Where I'm now? I don't know why", time: 169 },
-//  { text: "Nice butterflies in my hands", time: 176 },
-//  { text: "Too much light for twilight", time: 183 },
-//  { text: "In the mood for the flowers", time: 188 },
-//  { text: "Love.", time: 140 },
-//];
-
 var lyricsData = [
-  { text: "Estar contigo", time: 5 },
-  { text: "Hazme sentir de nuevo,", time: 10 },
-  { text: "Deslízate sobre mi piel de nuevo,", time: 15 },
-  { text: "Déjame descubrirte para redescubrirte", time: 20 },
-  { text: "Y me abriré si prometes ceder", time: 26 },
-  { text: "En esta noche perfecta", time: 32 },
-  { text: "Dejemos que los dos seamos uno...", time: 38 },
-  { text: "Seremos de nuevo en otro momento", time: 44 },
-  { text: "No importa lo que digan los demás", time: 50 },
-  { text: "Porque dejaría todo tan atrás", time: 56 },
-  { text: "Solo para estar contigo hoy...", time: 62 },
-  { text: "Así que hazme sentir de nuevo", time: 68 },
-  { text: "Sentir tu aliento de nuevo", time: 74 },
-  { text: "No importa nadie más", time: 80 },
-  { text: "Solo estamos tú y yo...", time: 86 },
-  { text: "Seremos de nuevo en otro momento", time: 92 },
-  { text: "No importa lo que digan los demás", time: 98 },
-  { text: "Porque dejaría todo tan atrás", time: 104 },
-  { text: "Solo para estar contigo hoy...", time: 110 },
-  { text: "Solo para estar contigo hoy", time: 116 },
-  { text: "Solo para estar contigo hoy", time: 122 },
-  { text: "Solo para estar contigo", time: 128 },
-  { text: "Seremos de nuevo en otro momento", time: 134 },
-  { text: "No importa lo que digan los demás", time: 140 },
-  { text: "Porque dejaría todo tan atrás", time: 146 },
-  { text: "Solo para estar contigo hoy...", time: 152 },
-  { text: "Así que seremos de nuevo en otro momento", time: 158 },
-  { text: "Y haré todo lo necesario para dejar a los demás", time: 164 },
-  { text: "Tan atrás", time: 170 },
-  { text: "Solo para poder estar, solo para poder estar...", time: 176 },
-  { text: "Contigo...", time: 182 },
-  { text: "Contigo...", time: 188 },
-  { text: "Contigo...", time: 194 },
-  { text: "Contigo...", time: 200 },
+  { text: "Hazme sentir de nuevo,", time: 18 },
+  { text: "Deslízate sobre mi piel de nuevo,", time: 25 },
+  { text: "Déjame despertarte para redescubrirte", time: 34 },
+  { text: "Y me abriré si prometes ceder", time: 45 },
+  { text: "En esta noche perfecta", time: 57 },
+  { text: "Dejemos que los dos seamos uno...", time: 62 },    // 1:02 = 62 seg
+  { text: "Estaremos juntos otra vez en otro momento", time: 70 }, // 1:10 = 70 seg
+  { text: "No importa lo que digan los demás", time: 77 },    // 1:17 = 77 seg
+  { text: "Porque dejaría todo tan atrás", time: 84 },        // 1:24 = 84 seg
+  { text: "Solo para estar contigo hoy...", time: 87 },       // 1:27 = 87 seg
+  { text: "Así que hazme sentir de nuevo", time: 97 },        // 1:37 = 97 seg
+  { text: "Sentir tu aliento de nuevo", time: 103 },          // 1:43 = 103 seg
+  { text: "No importa nadie más", time: 108 },                // 1:48 = 108 seg
+  { text: "Solo estamos tú y yo...", time: 116 },             // 1:56 = 116 seg
+  { text: "Estaremos juntos otra vez en otro momento", time: 123 }, // 2:03 = 123 seg
+  { text: "No importa lo que digan los demás", time: 130 },   // 2:10 = 130 seg
+  { text: "Porque dejaría todo tan atrás", time: 135 },       // 2:15 = 135 seg
+  { text: "Solo para estar contigo hoy...", time: 140 },      // 2:20 = 140 seg
+  { text: "Solo para estar contigo hoy", time: 146 },         // 2:26 = 146 seg
+  { text: "Solo para estar contigo hoy", time: 152 },         // 2:32 = 152 seg
+  { text: "Solo para estar contigo", time: 158 },             // 2:38 = 158 seg
+  { text: "Estaremos juntos otra vez en otro momento", time: 176 }, // 2:56 = 176 seg
+  { text: "No importa lo que digan los demás", time: 182 },   // 3:02 = 182 seg
+  { text: "Porque dejaría todo tan atrás", time: 186 },       // 3:06 = 186 seg
+  { text: "Solo para estar contigo hoy...", time: 191 },      // 3:11 = 191 seg
+  { text: "Así que Estaremos juntos otra vez otro momento", time: 195 }, // 3:15 = 195 seg
+  { text: "Y haré todo lo necesario para dejar a los demás", time: 205 },// 3:25 = 205 seg
+  { text: "Tan atrás", time: 209 },                           // 3:29 = 209 seg
+  { text: "Solo para poder estar, solo para poder estar...", time: 215 },// 3:35 = 215 seg
+  { text: "Contigo...", time: 223 },                          // 3:43 = 223 seg
+  { text: "Contigo...", time: 228 },                          // 3:48 = 228 seg
 ];
-
 // Animar las letras
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
